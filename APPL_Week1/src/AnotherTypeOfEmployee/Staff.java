8b2f4e64e40c5954c7ef6fff1c69b6287bdd61c0
dzakirafabillah <zigdzak27@gmail.com>
2021-03-15 11:37:56 +0700

    Update Staff.java
    Test Selesai

--- APPL_Week1/src/AnotherTypeOfEmployee/Staff.java
+++ APPL_Week1/src/AnotherTypeOfEmployee/Staff.java
@@ -43,28 +43,30 @@ public class Staff
  "555-8374");
  staffList[5] = new Volunteer ("Cliff", "321 Duds Lane",
  "555-7282");
- staffList[6] = new Commission ("Cliff", "321 Duds Lane",
- "555-7282", "128-56-1234");
- staffList[7] = new Commission ("Cliff", "321 Duds Lane",
- "555-7282", "118-27-7844");
+ staffList[6] = new Commission ("Dzakira", "Ujung Berung",
+ "511-7282", "128-56-1234", 6.25, 0.2);
+ staffList[7] = new Commission ("Fabillah", "Ciwaruga",
+ "534-6789", "118-27-7844", 9.75, 0.15);
  ((Executive)staffList[0]).awardBonus (500.00);
  ((Hourly)staffList[3]).addHours (40);
+ ((Commission)staffList[6]).addHours(35);
+ ((Commission)staffList[6]).addSales(400);
+ ((Commission)staffList[7]).addHours(40);
+ ((Commission)staffList[7]).addSales(950);
  }
  //-----------------------------------------------------------------
  // Pays all staff members.
  //-----------------------------------------------------------------
- public void payday ()
- {
- double amount;
- for (int count=0; count < staffList.length; count++)
- {
- System.out.println (staffList[count]);
- amount = staffList[count].pay(); // polymorphic
- if (amount == 0.0)
- System.out.println ("Thanks!");
- else
- System.out.println ("Paid: " + amount);
- System.out.println ("-----------------------------------");
- }
- }
+ public void payday (){
+    double amount;
+    for (int count=0; count < staffList.length; count++){
+        System.out.println (staffList[count]);
+        amount = staffList[count].pay(); // polymorphic
+        if (amount == 0.0)
+        System.out.println ("Thanks!");
+        else
+        System.out.println ("Paid: " + amount);
+        System.out.println ("-----------------------------------");
+    }
+    }
 }
