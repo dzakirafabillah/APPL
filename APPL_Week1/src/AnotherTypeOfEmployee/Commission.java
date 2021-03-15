244de390cc305400314e81e95a5e01941da839b0
dzakirafabillah <zigdzak27@gmail.com>
2021-03-15 11:09:51 +0700

    Update Commission.java
    Menambahkan attribute, constructor, dan method addSales

--- APPL_Week1/src/AnotherTypeOfEmployee/Commission.java
+++ APPL_Week1/src/AnotherTypeOfEmployee/Commission.java
@@ -9,6 +9,22 @@ package AnotherTypeOfEmployee;
  *
  * @author DZAKIRA
  */
-public class Commission {
+public class Commission extends Hourly{
+    double totalSales; //total sales the employee has made
+    double commissionRate; //commission rate (represent the percent in decimal form)
+    
+    /*Constructor*/
+    public Commission(String eName, String eAddress, String ePhone,
+                  String socSecNumber, double eRate, double eCommissionRate){
+        super(eName, eAddress, ePhone, socSecNumber, eRate);
+        this.commissionRate = eCommissionRate;
+    }
+    
+    /* method that adds the parameter to the instance variable representing 
+    total sales.*/
+    public void addSales (double totalSales){
+        this.totalSales += totalSales;
+    }
+    
     
 }
