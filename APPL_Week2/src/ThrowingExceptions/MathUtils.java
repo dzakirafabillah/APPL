15a320522f9357be8d981f1597b01bbcff5bb2ee
dzakirafabillah <zigdzak27@gmail.com>
2021-03-22 12:31:44 +0700

    persoalan3-step1(untuk n>0)

--- APPL_Week2/src/ThrowingExceptions/MathUtils.java
+++ APPL_Week2/src/ThrowingExceptions/MathUtils.java
@@ -15,16 +15,20 @@ package ThrowingExceptions;
 // Provides static mathematical utility functions.
 //
 // ****************************************************************
-public class MathUtils
-{
+public class MathUtils extends IllegalArgumentException{
 //-------------------------------------------------------------
 // Returns the factorial of the argument given
 //-------------------------------------------------------------
-public static int factorial(int n)
-{
-int fac = 1;
-for (int i=n; i>0; i--)
-fac *= i;
-return fac;
-}
+    public static int factorial (int n){
+        if(n<0){
+            throw new IllegalArgumentException("invalid input (kurang dari 0)");
+        }
+        else {
+            int fac = 1;
+            for (int i=n; i>0; i--)
+                fac *= i;
+            return fac;
+        }
+
+    }
 }
