af8219222de50ce6607abfc9f08d6540fcb6f8a8
dzakirafabillah <zigdzak27@gmail.com>
2021-03-22 11:27:26 +0700

    persoalan2-modify 2
    try and catch inside the loop

--- APPL_Week2/src/PlacingExceptionHandlers/ParseInts.java
+++ APPL_Week2/src/PlacingExceptionHandlers/ParseInts.java
@@ -28,15 +28,26 @@ public class ParseInts{
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
         
+        /*Modify 1 (false)*/
         /*try statement that encompasses the entire while loop.*/
-        try {
-            while (scanLine.hasNext()){
+//        try {
+//            while (scanLine.hasNext()){
+//                val = Integer.parseInt(scanLine.next());
+//                sum += val;
+//            }
+//        } catch (NumberFormatException e1) {
+//        }
+        
+        /*Modify 2 (true)*/
+        /*the try and catch inside the loop*/
+        while (scanLine.hasNext()){
+            try {
                 val = Integer.parseInt(scanLine.next());
                 sum += val;
+            } catch (NumberFormatException e1) {
             }
-        } catch (NumberFormatException e1) {
-        }
-        
+                
+         }
         
         System.out.println("The sum of the integers on this line is " + sum);
     }
