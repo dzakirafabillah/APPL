d6e8d92f208c288590385a8669ecf499574f18cf
dzakirafabillah <zigdzak27@gmail.com>
2021-03-22 10:56:51 +0700

    persoalan1-add try catch statement
    solved

--- APPL_Week2/src/ExceptionArentAlwaysError/CountLetters.java
+++ APPL_Week2/src/ExceptionArentAlwaysError/CountLetters.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ExceptionArentAlwaysError;
 
 /**
  *
  * @author DZAKIRA
  */
+
 // ****************************************************************
 // CountLetters.java
 //
@@ -30,7 +26,13 @@ public class CountLetters {
         //count frequency of each letter in string
         
         for (int i=0; i < word.length(); i++){
-                counts[word.charAt(i)-'A']++;            
+            //try if the character is between 'A' - 'Z'
+            try {
+                counts[word.charAt(i)-'A']++;
+            } catch (ArrayIndexOutOfBoundsException e1) {
+                //modify the body of the catch so that it prints a useful message
+                System.out.println("Not a Letter : " + word.charAt(i) );
+            }         
         }
             
         //print frequencies
